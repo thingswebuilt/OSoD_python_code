5e66ca7d6969933b5a59fc5d60c056639d1dd28a
markmellors <mark.mellors@gmail.com>
2022-02-08 15:17:34 +0000

    changed to using scikit-spatial, simple and works great

--- src/tof_tools.py
+++ src/tof_tools.py
@@ -3,9 +3,8 @@
 import functools
 import operator
 import numpy as np
-from numpy.linalg import svd
 import math
-from sklearn import linear_model
+from skspatial.objects import Plane
 
 def average_distance(sensor_reading: list[list[int]]) -> float:
     """returns the average value of all readings"""
@@ -18,7 +17,7 @@ def create_point_cloud(sensor_reading: list[list[int]]) -> list[tuple[float, flo
     """converts a 2D array of range measurements into a
      3d point cloud using standard VL53L5CX FOV,
      assuming the sensor is pointing vertically upwards"""
-    point_cloud=[[] for i in range(3)]
+    point_cloud=[]
     if len(sensor_reading) == 4:
         #sensor in 4x4 zone mapping
         #FOV is 45degree both vertically and horizontally
@@ -43,26 +42,13 @@ def create_point_cloud(sensor_reading: list[list[int]]) -> list[tuple[float, flo
             x = distance_value * math.sin(horizontal_angle)
             y = distance_value * math.sin(vertical_angle)
             z = distance_value * math.cos(horizontal_angle) * math.cos(vertical_angle)
-            point_cloud[0].append(x)
-            point_cloud[1].append(y)
-            point_cloud[2].append(z)
+            point_cloud.append([x, y, z])
     return point_cloud
 
 
-def plane_fit(sensor_reading: list[list[int]]) -> tuple[list[float],list[float]]:
-    """ use a lienar model to get a best fit plane of the sensor reading"""
-    # your data is stored as X, Y, Z
-    X, Y, Z = np.array(create_point_cloud(sensor_reading))
-
-    print(X.shape, Y.shape, Z.shape)
-
-    x1, y1, z1 = X.flatten(), Y.flatten(), Z.flatten()
-
-    X_data = np.array([x1, y1]).reshape((-1, 2))
-    Y_data = z1
-
-    reg = linear_model.LinearRegression().fit(X_data, Y_data)
-
-    print("coefficients of equation of plane, (a1, a2): ", reg.coef_)
-
-    print("value of intercept, c:", reg.intercept_)
+def plane_fit(sensor_reading: list[list[int]]) -> list[float]:
+    """ use a linear model to get a best fit plane of the sensor reading
+    returns normal vector """
+    points = create_point_cloud(sensor_reading)
+    plane = Plane.best_fit(points)
+    return plane.normal
