59eac2f4eb26894b77f96b5f5023828e9014cbf5
Rob Berwick <rob.berwick@gmail.com>
2022-02-13 15:14:48 +0000

    mmm tasty copypasta

--- tests/test_vl53l5cx.py
+++ tests/test_vl53l5cx.py
@@ -27,34 +27,34 @@ def test_1x1_dataframe_accepts_only_single_zone_reading():
 
 def test_4x4_dataframe_accepts_max_16_zone_readings():
     """test the 4x4 zone data model accepts a maximum of 16 zone readings"""
-    df_1x1 = Vl53l5cxDataFrame4x4()
+    df_4x4 = Vl53l5cxDataFrame4x4()
     for zone_index in range(1, 17):
         zone_reading = Vl53l5cxZoneReading(
             value=1, status=Vl53l5cxStatus.RANGE_VALID, zone=zone_index
         )
-        df_1x1.add_reading(zone_reading)
+        df_4x4.add_reading(zone_reading)
 
     with pytest.raises(ValueError):
         zone_reading_2 = Vl53l5cxZoneReading(
             value=1, status=Vl53l5cxStatus.RANGE_VALID, zone=2
         )
-        df_1x1.add_reading(zone_reading_2)
+        df_4x4.add_reading(zone_reading_2)
 
 
-def test_4x4_dataframe_accepts_max_64_zone_readings():
+def test_8x8_dataframe_accepts_max_64_zone_readings():
     """test the 8x8 zone data model accepts a maximum of 64 zone readings"""
-    df_1x1 = Vl53l5cxDataFrame8x8()
+    df_8x8 = Vl53l5cxDataFrame8x8()
     for zone_index in range(1, 65):
         zone_reading = Vl53l5cxZoneReading(
             value=1, status=Vl53l5cxStatus.RANGE_VALID, zone=zone_index
         )
-        df_1x1.add_reading(zone_reading)
+        df_8x8.add_reading(zone_reading)
 
     with pytest.raises(ValueError):
         zone_reading_2 = Vl53l5cxZoneReading(
             value=1, status=Vl53l5cxStatus.RANGE_VALID, zone=2
         )
-        df_1x1.add_reading(zone_reading_2)
+        df_8x8.add_reading(zone_reading_2)
 
 
 # def test_8x8_instantiates_correctly(perfect_flat_reading_data: list[int]):
